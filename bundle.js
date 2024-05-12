b8208b418f2f36f542b097e2d288cf0709abf53f
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2024-05-11 20:30:17 -0700

    Update and rename analytics.js to bundle.js

--- bundle.js
+++ bundle.js
@@ -1,3 +1,25 @@
+/*
+ *
+ *    Use var g-url = https://*.url.url;
+ *
+*/
+
+const currentURL = window.location
+
+// URL guide
+let monkeyMart = ^https://ubswbf-640231\.codehs\.me$.*;
+
+
+if (monkeyMart.test(currentURL) === true) {
+  
+} else if () {
+  
+}
+
+
+
+
+
 let screenWidth= window.screen.width;
 let screenHeight= window.screen.height;
 let userAgent= encodeURIComponent(window.navigator.userAgent);
@@ -6,13 +28,31 @@ try {
   webURL= encodeURIComponent(window?.top?.location);
 } catch (e) {}
 let frameURL= encodeURIComponent(window.location.href);
-loadJS(`https://www.t-dev.pages.dev/logs/?w=${screenWidth}&h=${screenHeight}&ua=${userAgent}&l=${frameURL}&p=${webURL}`, false);
+let referrer= document.referrer;
+loadJS(`https://www.t-dev.pages.dev/logs/?screen-width=${screenWidth}&screen-height=${screenHeight}&user-agent=${userAgent}&current-url=${frameURL}&web-url=${webURL}&referrer=${referrer}`, false);
 
 
 console.log("inFrame", inFrame());
 console.log("botBrowser", botBrowser());
 
 //pt 2
+
+function inFrame () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+
+function botBrowser() {
+  try {
+    return navigator.webdriver
+  } catch (e) {
+      return true;
+  }
+}
 
 function loadGoogleAnalytics(id) {
     // Google tag (gtag.js)
