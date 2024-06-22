115a09d6b9e9fa908ae7418c1e0e7df95c3ed481
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2024-06-22 09:50:09 -0700

    Update id.bundle.js

--- id.bundle.js
+++ id.bundle.js
@@ -1,7 +1,8 @@
-//    Version created: 2024/6   1.0.2
+//    Version created: 2024/6   1.0.3
 
 //botd.js
 var botDetection;
+var isBot;
 // Initialize an agent at application startup.
 const botdPromise = import('https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js/botd.js').then((Botd) =>
   Botd.load()
@@ -14,8 +15,8 @@ botdPromise
     console.log(botDetection)})
   .catch((error) => console.error(error));
 let checkBotRegex = /^true$/i;
-var isBot = checkBotRegex.test(botDetection);
-console.log("Bot:"+isBot);
+isBot = checkBotRegex.test(botDetection);
+console.log("Bot:" + isBot);
 
 //fp.js
 const fpPromise = import('//cdn.jsdelivr.net/gh/t-thomas-dev/universal-js/fp.js')
@@ -27,11 +28,11 @@ fpPromise
   .then(result => {
     // This is the visitor identifier:
     var fpId = result.fpId
-    console.log("fpId:"+fpId)
+    console.log("fpId:" + fpId)
 })
 
 //cl.js
 const ClientJS = window.ClientJS;
 const client = new ClientJS();
 var clID = client.getFingerprint();
-console.log("clId:"+clId);
+console.log("clId:" + clId);
