25125cfb2c2b4baad76eec50aa3fc13fdbae3622
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2024-07-31 17:50:38 -0700

    Update auth.bundle.js

--- auth.bundle.js
+++ auth.bundle.js
@@ -1,63 +1,64 @@
-//    Version: 2024/6   1.0.2
+//    Version: 2024/7   2.0.0
 
 //setup
-const dateTime = currentDate.toLocaleString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-  hour12: false,
-});
-
 function createCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+var expires = "";
+if (days) {
+  var date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  expires = "; expires=" + date.toUTCString();
+}
+document.cookie = name + "=" + (value || "") + expires + ";domain=codehs.me";
 }
 
 function isChromeOS() {
-  return /CrOS/.test(navigator.userAgent);
+return /CrOS/.test(navigator.userAgent);
 }
 const isChromeOSUser = isChromeOS();
 
 function hasCookie(name) {
-  return (document.cookie.split('; ').indexOf(name + '=') !== -1);
-}
+  // Get all cookies
+  const cookies = document.cookie.split(';');
 
-const hasAuthCookie = hasCookie(auth);
+  // Loop through each cookie
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+
+    // Check if cookie name starts with "auth="
+    if (cookie.indexOf(`${name}=`) === 0) {
+        return true;
+    }
+  }
+  // No cookies enabled or auth cookie not found
+  return false;
+}
 
 //code that is ran
 //check if cookies are allowed
-if (hasCookie(auth)) {
-  location.replace(`https://example.com/already-authed`);
-}
 if (!navigator.cookieEnabled) {
-    window.location.replace("//www.example.com/enable-cookies");
+  window.location.replace(`/enable-cookies.html`);
 }
 //check for cookie
-if (hasSessionCookie === true) {
-  location.replace(`https://example.com/has-cookie`);
+if (hasCookie("auth")) {
+  window.location.replace(`/has-cookie.html`);
 } else {
   //check for chrome os
   if (isChromeOSUser === true) {
-    createCookie("session", "Created-" + dateTime + "_userAgent-"+ navigator.userAgent + "_Languages-" + navigator.languages, 180);
-    location.replace(`https://example.com/success`);
+    createCookie("auth", `1`, 180);
+    alert(" ✅ Success! You will be logged in for 180 days")
+    location.replace(`/success.html`);
   } else {
     //check pin
-    const PINinput = prompt("Enter your PIN")
-    if (PINinput === 36158) {
-      createCookie("session", "Created-" + dateTime + "_userAgent-"+ navigator.userAgent + "_Languages-" + navigator.languages, 180);
-    } else {
-      //if user enters incorrect pin
-      const failCount += 1;
-      createCookie("challengeFail, "Created-" + dateTime + "_userAgent-"+ navigator.userAgent + "_Languages-" + navigator.languages, 360);
-      location.replace(`https://example.com/landing`);
-    }
+    // const PINinput = prompt("Enter your PIN")
+    // if (PINinput === '365158') {
+    //   createCookie("auth", `1`, 180);
+    //   alert(" ✅ Success! You will be logged in for 180 days")
+    //   window.location.replace(`/landing.html`);
+    // } else {
+    //   //if user enters incorrect pin
+      createCookie("accessBlock", `1`, 720);
+      newrelic.log(`session has access block--${sessionId}`, {level: 'warn'})
+      window.location.replace(`/403.html`);
+    // }
   }
 }
