912aefd82bcddf380a5ff3b75801bb1cd6a74b53
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2024-07-31 17:57:13 -0700

    Update id.bundle.js

--- id.bundle.js
+++ id.bundle.js
@@ -1,10 +1,17 @@
-//    Version created: 2024/6   1.0.5
+//    Version created: 2024/7   2.0.0
 
-//botd.js
+var sessionId = self.crypto.randomUUID();
+  console.log(`sessionId:${sessionId}`);
 var botDetection;
 var isBot;
+var fpId;
+var clId;
+var rawUserData;
+var userData;
+
+//botd.js
 // Initialize an agent at application startup.
-const botdPromise = import('https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js@master/botd.js').then((Botd) =>
+botdPromise = import('https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js/botd.js').then((Botd) =>
   Botd.load()
 );
 // Get the bot detection result when you need it.
@@ -16,24 +23,20 @@ botdPromise
   .catch((error) => console.error(error));
 let checkBotRegex = /^true$/i;
 isBot = checkBotRegex.test(botDetection);
-console.log("Bot:" + isBot);
 
-//fp.js
-const fpPromise = import('https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js@master/fp.js')
-  .then(FingerprintJS => FingerprintJS.load())
-
-// Get the visitor identifier when you need it.
-fpPromise
-  .then(fp => fp.get())
-  .then(result => {
-    // This is the visitor identifier:
-    var fpId = result.fpId
-    console.log("fpId:" + fpId)
-})
-
-//cl.js
-const clPromise = import('https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js@master/cl.js')
-const ClientJS = window.ClientJS;
-const client = new ClientJS();
-var clID = client.getFingerprint();
-console.log("clId:" + clId);
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "complete") {
+    //finish botd.js
+    console.log("Bot:" + isBot);
+    //fp.js
+    const fpjsScriptWrapper = document.createElement('script');
+      fpjsScriptWrapper.innerHTML = 'var fpId; fpPromise = import(`https://cdn.jsdelivr.net/gh/t-thomas-dev/universal-js/fp.js`) .then(FingerprintJS => FingerprintJS.load()); fpPromise.then(fp => fp.get()) .then(result => {fpId = result.visitorId; console.log("fpId:" + fpId)})';
+    document.body.appendChild(fpjsScriptWrapper)
+    
+    //cl.js
+    const ClientJS = window.ClientJS;
+    const client = new ClientJS();
+    clId = client.getFingerprint();
+    console.log("clId:" + clId);
+  }
+});
